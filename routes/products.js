0758cb26d529a432baa50196e299fed5922d8bd0
KBakaldina <kr.bakaldina@gmail.com>
2019-10-24 18:57:58 +0300

    showing current user his products (not done)

--- routes/products.js
+++ routes/products.js
@@ -1,11 +1,20 @@
 const express = require('express');
 const router = express.Router();
+const jwt = require('jsonwebtoken');
+const queryPromise = require('../libs/dbConnection').queryPromise;
 
 /* GET product page. */
-router.get('/', (req, res) => {
-    if(req.cookies.token)
-        res.render('products');
-    else res.redirect('login');
+router.get('/', async (req, res) => {
+    if(req.cookies.token) {
+        try {
+            let rows = await queryPromise(
+                'SELECT * FROM products WHERE userId=?',
+                jwt.verify(req.cookies.token, process.env.JWT_SECRET).id);
+            res.render('products', {rows: rows});
+        } catch(err) {
+            res.render('error', {message: 'Ooops...', error: err});
+        }
+    } else res.redirect('login');
 });
 
 module.exports = router;
