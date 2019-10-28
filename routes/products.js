b8e6c6eadb475096aa993e443b432af4a9017a32
KBakaldina <kr.bakaldina@gmail.com>
2019-10-28 19:01:54 +0300

    just commit

--- routes/products.js
+++ routes/products.js
@@ -4,7 +4,7 @@ const passport = require('passport');
 const jwt = require('jsonwebtoken');
 const queryPromise = require('../libs/dbConnection').queryPromise;
 const actionAddProduct = require('../actions/products/add');
-const actionUpdateProduct = require('../actions/products/edit');
+const actionEditProduct = require('../actions/products/edit');
 const actionDeleteProduct = require('../actions/products/delete');
 
 /* GET product page. */
@@ -32,48 +32,66 @@ router.get('/add', (req, res) => {
 });
 
 /* POST add new product page. */
-router.post('/add',  async (req, res) => {
-    try {
-        await actionAddProduct(
-            req.body.productName, req.body.description, req.body.picLink,
-            jwt.verify(req.cookies.token, process.env.JWT_SECRET).id);
-        res.redirect('/products')
-    } catch(err) { res.send(err);}
-});
-
-/* GET update product page. */
-router.get('/update', (req, res) => {
+router.post('/add', (req, res) => {
     passport.authenticate('jwt', {session: false}, async (err, user) => {
         if (user) {
-            //TODO: передать rows из products.ejs(прочитать из req?)
+            try {
+                await actionAddProduct(
+                    req.body.productName, req.body.description, req.body.picLink,
+                    jwt.verify(req.cookies.token, process.env.JWT_SECRET).id);
+                res.redirect('/products')
+            } catch(err) { res.send(err);}
+        } else if (user == false && err === null) return res.redirect('login');
+        else return res.render('error', {message: 'Wow! Something\'s wrong...', error: err});
+    })(req, res);
+});
+
+/* GET edit product page. */
+router.get('/edit', (req, res) => {
+    passport.authenticate('jwt', {session: false}, async (err, user) => {
+        if (user) {
             try {
                 let rows = await queryPromise(
                     'SELECT * FROM products WHERE userId=?',
                     jwt.verify(req.cookies.token, process.env.JWT_SECRET).id);
-                res.render('products/update', {rows: rows});
-            } catch (err) {
-                res.render('error', {message: 'Ooops...', error: err});
-            }
+                res.render('products/edit', {rows: rows});
+            } catch (err) { res.render('error', {message: 'Ooops...', error: err}); }
         } else if (user == false && err === null) return res.redirect('login');
         else return res.render('error', {message: 'Wow! Something\'s wrong...', error: err});
     })(req, res);
 });
 
 //TODO:
-/* POST update product page. */
-router.post('/update/:id()/:name', (req, res) => {
-    //req.params.id
-    res.send("product was updated");
+/* POST edit product page. */
+//'/edit/:id(pattern)/:name'
+router.get('/edit/:id', (req, res) => {
+    passport.authenticate('jwt', {session: false}, async (err, user) => {
+        if (user) {
+            try {
+                let rows = await queryPromise(
+                    'SELECT * FROM products WHERE userId=?',
+                    jwt.verify(req.cookies.token, process.env.JWT_SECRET).id);
+                res.render('products/products', {rows: rows});
+            } catch(err) { res.render('error', {message: 'Ooops...', error: err}); }
+            res.render('products/edit', {rows: rows});
+        } else if (user == false && err === null) return res.redirect('login');
+        else return res.render('error', {message: 'Wow! Something\'s wrong...', error: err});
+    })(req, res);
 });
 
 /* POST delete product page. */
-router.get('/delete/:id', async (req, res) => {
-    try {
-        console.log(req.params.id);
-        await actionDeleteProduct(
-            req.params.id);
-        res.redirect('/products');
-    } catch(err) { res.send(err);}
+router.get('/delete/:id', (req, res) => {
+    passport.authenticate('jwt', {session: false}, async (err, user) => {
+        if (user) {
+            try {
+                console.log(req.params.id);
+                await actionDeleteProduct(
+                    req.params.id);
+                res.redirect('/products');
+            } catch(err) { res.send(err);}
+        } else if (user == false && err === null) return res.redirect('login');
+        else return res.render('error', {message: 'Wow! Something\'s wrong...', error: err});
+    })(req, res);
 });
 
 module.exports = router;
