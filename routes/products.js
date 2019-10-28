dbf63e5d9aa688f4373dcba0e63d01e0419bc0ae
KBakaldina <kr.bakaldina@gmail.com>
2019-10-28 18:06:37 +0300

    just commit

--- routes/products.js
+++ routes/products.js
@@ -43,11 +43,26 @@ router.post('/add',  async (req, res) => {
 
 /* GET update product page. */
 router.get('/update', (req, res) => {
-    res.send('update page');
+    passport.authenticate('jwt', {session: false}, async (err, user) => {
+        if (user) {
+            //TODO: передать rows из products.ejs(прочитать из req?)
+            try {
+                let rows = await queryPromise(
+                    'SELECT * FROM products WHERE userId=?',
+                    jwt.verify(req.cookies.token, process.env.JWT_SECRET).id);
+                res.render('products/update', {rows: rows});
+            } catch (err) {
+                res.render('error', {message: 'Ooops...', error: err});
+            }
+        } else if (user == false && err === null) return res.redirect('login');
+        else return res.render('error', {message: 'Wow! Something\'s wrong...', error: err});
+    })(req, res);
 });
 
+//TODO:
 /* POST update product page. */
-router.post('/update', (req, res) => {
+router.post('/update/:id()/:name', (req, res) => {
+    //req.params.id
     res.send("product was updated");
 });
 
@@ -55,7 +70,7 @@ router.post('/update', (req, res) => {
 router.get('/delete', (req, res) => {
     passport.authenticate('jwt', {session: false}, async (err, user) => {
         if (user) {
-            //TODO: можно ли передать rows из products.ejs(прочитать из req?)
+            //TODO: передать rows из products.ejs(прочитать из req?)
             try{
                 let rows = await queryPromise(
                     'SELECT * FROM products WHERE userId=?',
