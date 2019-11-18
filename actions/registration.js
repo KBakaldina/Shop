3c2736bbfc36f5b17d083dd8eb56182c3930d32d
KBakaldina <kr.bakaldina@gmail.com>
2019-11-18 16:51:58 +0300

    checking is email unique while registration

--- actions/registration.js
+++ actions/registration.js
@@ -5,18 +5,24 @@ module.exports = async  (userName, email, userPassword, userPassword2) => {
     let rows = await queryPromise('SELECT * FROM users WHERE userName = ?', userName);
 
     if (!rows[0]) {
-        if(userPassword === userPassword2){
-            userPassword = bcrypt.hashSync(userPassword, process.env.HASH_SALT);
+        rows = await queryPromise(
+            'SELECT * FROM users WHERE email = ?',
+            [email]);
 
-            await queryPromise(
-                'INSERT INTO users(userName, password, email) VALUES (?, ?, ?)',
-                [userName, userPassword, email]);
+        if (!rows[0]) {
+            if(userPassword === userPassword2){
+                userPassword = bcrypt.hashSync(userPassword, process.env.HASH_SALT);
 
-            rows = await queryPromise(
-                'SELECT id FROM users WHERE userName = ?',
-                [userName]);
+                await queryPromise(
+                    'INSERT INTO users(userName, password, email) VALUES (?, ?, ?)',
+                    [userName, userPassword, email]);
 
-            return {id: rows[0].id, msg: 'Success'};
-        } return {id: false, msg: 'Incorrect conformation of password. Try again, please!'};
+                rows = await queryPromise(
+                    'SELECT id FROM users WHERE userName = ?',
+                    [userName]);
+
+                return {id: rows[0].id, msg: 'Success'};
+            } return {id: false, msg: 'Incorrect conformation of password. Try again, please!'};
+        } else return {id: false, msg: 'This email is already in use. Please, choose another email and try again or login!'};
     } else return {id: false, msg: 'This user already exists. Please, choose another name and try again!'};
 };
