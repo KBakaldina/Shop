424d34e61eed8d033da722febd9ca7f7f8c0658b
KBakaldina <kr.bakaldina@gmail.com>
2019-12-05 19:23:48 +0300

    try #2 of an unit test (jest) : done wrong

--- tests/products_service.test.js
+++ tests/products_service.test.js
@@ -1,15 +1,22 @@
 const actionAddProduct = require ('../actions/products/add');
 
-//TODO: a mock/stub/spy for the database?
+actionAddProduct.queryPromise = jest.fn((query, data) => {
+    return new Promise((resolve, reject) => {
+        console.log(data);
+        return reject('err');
+    });//DOES NOT WORK
+});
+
 describe ('Products service', () => {
-    describe ('The process of adding a new product', () => {
+    describe ('The action of adding a new product', () => {
+        let paramNum = 1;
         test.each([
             [undefined, 1.11, 'description 1', '/uploads/pic1', 1],
             ['Second', undefined, 'description 2', '/uploads/pic2', 2],
             ['Third', 3.33, undefined, '/uploads/pic3', 3],
             ['Fourth', 4.44, 'description 4', undefined, 4],
             ['Fifth', 5.55, 'description 5', '/uploads/pic5', undefined],
-        ])('If any parameter is undefined, the action should throws an error',
+        ])(`If any parameter of data(%s, %d, %s, %s, %d) is undefined, the action should throw an error`,
             async (productName, price, description, pictureLink, userId) => {
             let thrownError = false;
 
