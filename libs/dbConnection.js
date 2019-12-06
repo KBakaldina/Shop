e90eac9b7ec401bf34b565db50c6909ef1589f22
KBakaldina <kr.bakaldina@gmail.com>
2019-12-06 19:08:43 +0300

    database connection bug fixed (if table does not exist, it`s foreign keys can not be created)

--- libs/dbConnection.js
+++ libs/dbConnection.js
@@ -6,57 +6,60 @@ const connection = mysql.createPool({
     password: process.env.DB_PASS,
     database: process.env.DB_NAME
 });
-if (connection) {
 
 //TODO: what to do with console.log?
-connection.getConnection( (err) => {
-    if (err) console.log(err);
-    else {
-        connection.query('CREATE TABLE IF NOT EXISTS users (' +
-            'id INT(10) UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY, ' +
-            'userName VARCHAR(20) UNIQUE KEY NOT NULL, ' +
-            'password VARCHAR(100), ' +
-            'email VARCHAR(100) UNIQUE KEY, ' +
-            'pictureLink VARCHAR(200))', (err) => {
-            if (err) console.log(err);
-        });
+if (connection) {
+    connection.getConnection( (err) => {
+        if (err) console.log(err);
+        else {
+            connection.query('CREATE TABLE IF NOT EXISTS users (' +
+                'id INT(10) UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY, ' +
+                'userName VARCHAR(20) UNIQUE KEY NOT NULL, ' +
+                'password VARCHAR(100), ' +
+                'email VARCHAR(100) UNIQUE KEY, ' +
+                'pictureLink VARCHAR(200))', (err) => {
+                if (err) console.log(err);
+                else {
+                    connection.query('CREATE TABLE IF NOT EXISTS products (' +
+                        'id INT(10) UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY, ' +
+                        'productName VARCHAR(25) NOT NULL, ' +
+                        'price DECIMAL(9,2) NOT NULL, ' +
+                        'description VARCHAR(300) NOT NULL, ' +
+                        'pictureLink VARCHAR(200), ' +
+                        'userId INT(10) UNSIGNED NOT NULL, ' +
+                        'CONSTRAINT FK_products_userId FOREIGN KEY (userId) REFERENCES users(id) ON DELETE CASCADE)', (err) => {
+                        if (err) console.log(err);
+                        else {
+                            connection.query('CREATE TABLE IF NOT EXISTS likes (' +
+                                'productId INT(10) UNSIGNED NOT NULL,' +
+                                'CONSTRAINT FK_likes_productId FOREIGN KEY (productId) REFERENCES products(id) ON DELETE CASCADE, ' +
+                                'userId INT(10) UNSIGNED NOT NULL, ' +
+                                'CONSTRAINT FK_likes_userId FOREIGN KEY (userId) REFERENCES users(id) ON DELETE CASCADE, '+
+                                'PRIMARY KEY (productId, userId))', (err) => {
+                                if (err) console.log(err);
+                            });
+                        }
+                    });
 
-        connection.query('CREATE TABLE IF NOT EXISTS products (' +
-            'id INT(10) UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY, ' +
-            'productName VARCHAR(25) NOT NULL, ' +
-            'price DECIMAL(9,2) NOT NULL, ' +
-            'description VARCHAR(300) NOT NULL, ' +
-            'pictureLink VARCHAR(200), ' +
-            'userId INT(10) UNSIGNED NOT NULL, ' +
-            'CONSTRAINT FK_products_userId FOREIGN KEY (userId) REFERENCES users(id) ON DELETE CASCADE)', (err) => {
-            if (err) console.log(err);
-        });
+                    connection.query('CREATE TABLE IF NOT EXISTS tokens (' +
+                        'userId INT(10) UNSIGNED NOT NULL, ' +
+                        'token VARCHAR(500) NOT NULL, ' +
+                        'CONSTRAINT FK_tokens_userId FOREIGN KEY (userId) REFERENCES users(id) ON DELETE CASCADE)', (err) => {
+                        if (err) console.log(err);
+                    });
 
-        connection.query('CREATE TABLE IF NOT EXISTS likes (' +
-            'productId INT(10) UNSIGNED NOT NULL,' +
-            'CONSTRAINT FK_likes_productId FOREIGN KEY (productId) REFERENCES products(id) ON DELETE CASCADE, ' +
-            'userId INT(10) UNSIGNED NOT NULL, ' +
-            'CONSTRAINT FK_likes_userId FOREIGN KEY (userId) REFERENCES users(id) ON DELETE CASCADE, '+
-            'PRIMARY KEY (productId, userId))', (err) => {
-            if (err) console.log(err);
-        });
+                    connection.query('CREATE TABLE IF NOT EXISTS facebook (' +
+                        'fbId BIGINT(40) UNSIGNED NOT NULL PRIMARY KEY, ' +
+                        'userId INT(10) UNSIGNED NOT NULL, ' +
+                        'token VARCHAR(500) NOT NULL, ' +
+                        'CONSTRAINT FK_facebook_userId FOREIGN KEY (userId) REFERENCES users(id) ON DELETE CASCADE)', (err) => {
+                        if (err) console.log(err);
+                    });
+                }
 
-        connection.query('CREATE TABLE IF NOT EXISTS tokens (' +
-            'userId INT(10) UNSIGNED NOT NULL, ' +
-            'token VARCHAR(500) NOT NULL, ' +
-            'CONSTRAINT FK_tokens_userId FOREIGN KEY (userId) REFERENCES users(id) ON DELETE CASCADE)', (err) => {
-            if (err) console.log(err);
-        });
-
-        connection.query('CREATE TABLE IF NOT EXISTS facebook (' +
-            'fbId BIGINT(40) UNSIGNED NOT NULL PRIMARY KEY, ' +
-            'userId INT(10) UNSIGNED NOT NULL, ' +
-            'token VARCHAR(500) NOT NULL, ' +
-            'CONSTRAINT FK_facebook_userId FOREIGN KEY (userId) REFERENCES users(id) ON DELETE CASCADE)', (err) => {
-            if (err) console.log(err);
-        });
-    }
-});
+            });
+        }
+    });
 }
 
 const queryPromise = (query, data) => {
--- tests/action_registration.test.js
+++ tests/action_registration.test.js
@@ -15,7 +15,6 @@ describe ('User service', () => {
     });
 
     describe ('The action of registration a new user', () => {
-
         it('should throw an error if userName is already exists in the database', async () => {
             const actionRegistration = require('../actions/registration');
             expect(actionRegistration()).rejects.toThrow();
